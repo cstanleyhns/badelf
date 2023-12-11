4c001274f8bd8c759e8470827e687449d0ebe169
cstanleyhns <cstanleyhns>
2023-12-11 11:26:28 +0000

    added snow and backdrop

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import elfBanner from './assets/elf-banner.png'
 import './App.css'
+import { Snowflakes } from './Snowflakes'
+import { motion, useScroll } from "framer-motion"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <Snowflakes />
+      <motion.div
+       initial={{y: "-30px", scale: 0.5 }}
+       whileInView={{y: "0px", scale: 1 }}
+       transition={{ duration: 0.5, ease: "easeOut" }}>
+          <img src={elfBanner} className="logo" alt="Vite logo" />
+      </motion.div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
---
+++ src/Snowflakes.jsx
@@ -0,0 +1,48 @@
+import './Snowflakes.css'
+
+export function Snowflakes() {
+
+
+    return (
+        <>
+           <div className="snowflakes" aria-hidden="true">
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            <div className="snowflake">
+                <div className="inner">❅</div>
+            </div>
+            </div>
+        </>
+    )   
+}
