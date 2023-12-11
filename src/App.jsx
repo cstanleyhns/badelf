09b5a8e03e1610a570a9014932cb48e9ac292e0b
cstanleyhns <cstanleyhns>
2023-12-11 22:59:04 +0000

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <>
-      <Snowflakes />
+      {/* <Snowflakes /> */}
       {/* <Navbar /> */}
       <div className='topImageContainer'>
         <div className='topHeading'>$BADELF</div>
