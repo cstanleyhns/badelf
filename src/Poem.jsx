46965428efc967398f7581013b1a846da6c6e45e
cstanleyhns <cstanleyhns>
2023-12-12 08:47:12 +0000

    more animation

--- src/Poem.jsx
+++ src/Poem.jsx
@@ -3,7 +3,7 @@ import elfStanding from './assets/elf-standing.png'
 import elfSpanked from './assets/elf-spanked.png'
 import elfBlow from './assets/elf-blow.png'
 import elfSick from './assets/elf-sick.png'
-import { motion, useScroll } from "framer-motion"
+import { delay, motion } from "framer-motion"
 
 export function Poem() {
 
@@ -12,8 +12,13 @@ export function Poem() {
           scale: [1, 1.2, 1],
           y: [0, 10, 0],
           transition: { duration: 2, ease: "easeInOut", repeat: Infinity }
-        }
-      }
+        },
+        slideImages: {
+            scale: [1, 1.2, 1],
+            y: [0, 10, 0],
+            transition: { duration: 2, ease: "easeInOut" }
+          }
+    }
 
     return (
         <>
@@ -21,7 +26,11 @@ export function Poem() {
                 <div className='poemContainer'>
                   <div className='poemTitle'><span className='highlight'>Once upon a time...</span></div>
                   <div className='poemItem'>
-                    <img className='poemImage' src={elfStanding} alt='elf' />
+                    <motion.img
+                        whileInView={
+                            { scale: [1.2,1], delay: 0.5, transition: { duration: 2, ease: "easeInOut" }}
+                        }
+                        className='poemImage' src={elfStanding} alt='elf' />
                     <div className='poemCopy'>In the moon's soft, silvery light,
                                     A naughty elf partied through the night. 
                                     With cans of lager and a sack of blow,
@@ -32,10 +41,18 @@ export function Poem() {
                                         After eating magic mushrooms his senses were torn.
                                         Romanced Barbie dolls, in a fanciful spree,
                                         Did he put a condom on? No, don’t be silly.</div>  
-                                    <img className='poemImage' src={elfSpanked} alt='elf' />
+                    <motion.img 
+                            whileInView={
+                                { scale: [1.2,1], delay: 0.5, transition: { duration: 2, ease: "easeInOut" }}
+                            }
+                            className='poemImage' src={elfSpanked} alt='elf' />
                     </div>
                     <div className='poemItem'>
-                        <img className='poemImage' src={elfSick} alt='elf' />   
+                        <motion.img 
+                            whileInView={
+                                { scale: [1.2,1], delay: 0.5, transition: { duration: 2, ease: "easeInOut" }}
+                            }
+                            className='poemImage' src={elfSick} alt='elf' />   
                         <div className='poemCopy'>So with a throbbing head and a sense of dread,
                                     He set to work, no more to be said.
                                     Gathering gifts with a magical touch,
@@ -58,67 +75,13 @@ export function Poem() {
                     </div>
                     <div className='poemItemFinal'>
                          <motion.img 
-                                  variants={variants}
-                                  animate="slide"
+                            variants={variants}
+                            animate="slide"
                          className='poemImageFinal' src={elfBlow} alt='elf' />   
                     </div>
                 </div>
             </div>
-            {/* <div className='poemWrapper'>
-                <div className='poemContainer'>
-   
-                 
-                    <div className='poemTitle'><span className='highlight'>Once upon a time...</span></div>
-                    <div className='poemItem'>
-                    <img className='poemImage' src={elfStanding} alt='elf' />
-                    <div className='poemCopy'>In the moon's soft, silvery light,
-                                    A naughty elf partied through the night. 
-                                    With cans of lager and a sack of blow,
-                                    He partied and danced in the snow.</div>  
-                    </div>
-                    <div className='poemItem'>
-                    <div className='poemCopy'>He drank with vigor, from dusk till dawn,
-                                        After eating magic mushrooms his senses were torn.
-                                        Romanced Barbie dolls, in a fanciful spree,
-                                        Did he put a condom on? No, don’t be silly.</div>  
-                                    <img className='poemImage' src={elfSpanked} alt='elf' />
-                    </div>
-                    <div className='poemItem'>
            
-                    <div className='poemCopy'>But his mischief led to a dire plight,
-                                For he ruined Christmas, oh, what a sight!
-                                Toys scattered, stockings empty, a terrible fray,
-                            He had to fix this before the break of day.</div>  
-
-                    </div>
-                    <div className='poemItem'>
-                    <img className='poemImage' src={elfSick} alt='elf' />   
-                    <div className='poemCopy'>So with a throbbing head and a sense of dread,
-                                He set to work, no more to be said.
-                                Gathering gifts with a magical touch,
-                                For he owed the children that much.</div>  
-
-                    </div>
-                    <div className='poemItem'>
-
-                    <div className='poemCopy'>Through every chimney, at every bed,
-                                He left presents, just as Santa said.
-                                Toys and candies, joy and cheer,
-                                To make amends for his blunder last year.</div>  
-
-                    </div>
-                    <div className='poemItem'>
-                    <div className='poemCopy'>As the sun rose and the children smiled,
-                                The elf felt a warmth, tender and mild.
-                                Despite his hangover, and the night's wild mess,
-                                He'd brought back Christmas, no more, no less.</div>  
-                    
-                    </div>
-                    <div className='poemItemFinal'>
-                         <img className='poemImageFinal' src={elfBlow} alt='elf' />   
-                    </div>
-                </div>
-            </div> */}
         </>
     )   
 }
